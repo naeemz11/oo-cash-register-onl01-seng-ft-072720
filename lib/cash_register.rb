d7d4dee9922ca8cf0aed476ad34114c67de44664
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-07 04:05:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,7 +34,10 @@ class CashRegister
   end
 
   def void_last_transaction
-    self.total = @total - @transactions.pop
+    self.total -= self.last_transaction[1] * self.last_transaction[2]
+    self.last_transaction[2].times do
+      self.items.delete_at(self.items.index(self.last_transaction[0]) || self.items.count)
+    end
   end
 end
   
